a4c8ce4766aefb62676b69d950c39c42f55bad25
ajf8024 <afrycscs@gmail.com>
2024-04-24 09:03:49 -0400

    Finished Code

---
+++ CT1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Declare and initialize variables for a fictional person
+    std::string firstName = "Micheal";
+    std::string lastName = "Jordan";
+    std::string streetAddress = "1234 Loony Toons Ave";
+    std::string city = "Space Mountain";
+    std::string zipCode = "90210";
+
+    // Output the person's information to the console
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
