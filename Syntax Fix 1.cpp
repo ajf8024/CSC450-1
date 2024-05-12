d448babda8a071d1625862fad2e4a8bcbb235305
ajf8024 <afrycscs@gmail.com>
2024-05-12 17:04:38 -0400

    Finished code

---
+++ Syntax Fix 1.cpp
@@ -0,0 +1,25 @@
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+
+#include <iostream>
+// Removed #include <conio.h> as it's not necessary for this simple example
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main() {
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Wait For Output Screen
+    cout << "Press enter to continue...";
+    cin.get();  // Waits for user to press Enter
+
+    // Main Function return Statement
+    return 0;
+}
+
---
+++ Syntax Fix 2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+// Removed #include<conio.h> as it's not necessary for this simple example
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main() {
+
+    double myMoney = 1000.50;  // this should be printed out
+
+    // Standard Output Statement
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait For Output Screen
+    cout << "Press enter to continue...";
+    cin.get();  // Waits for user to press Enter
+
+    // Main Function return Statement
+    return 0;
+}
+
